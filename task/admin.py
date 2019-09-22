91c67e73f8377acef8e4f2475d66dd8144cabe58
CoffeBee <40368492+CoffeBee@users.noreply.github.com>
2019-09-22 16:17:39 +0300

    add Theme model to admin

--- task/admin.py
+++ task/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Task, Solution
+from .models import Task, Theme
 # Register your models here.
 admin.site.register(Task)
+admin.site.register(Theme)

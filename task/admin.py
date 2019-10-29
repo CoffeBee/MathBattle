c43c395a20dfc0bbd257a66040048dc287f16fc7
CoffeBee <ivan.podvorniy@yandex.ru>
2019-10-29 18:50:58 +0300

    fix trash

--- task/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Task, Theme, TaskAdmin, ThemeAdmin
-# Register your models here.
-admin.site.register(Task, TaskAdmin)
-admin.site.register(Theme, ThemeAdmin)
--- task/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class NumSolveForm(forms.Form):
-
-    answer = forms.CharField(max_length=100, label="You answer")

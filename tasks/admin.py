60fc0a521bcbbbd6c472a42ac6eb9a67c7ba9abf
CoffeBee <40368492+CoffeBee@users.noreply.github.com>
2019-11-09 14:02:42 +0300

    add globa admin panel  2

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Task, Theme, TaskAdmin, ThemeAdmin, Solution, Contest, ContestAdmin
+from .models import Task, Theme, TaskAdmin, ThemeAdmin, Solution, Contest, ContestAdmin, GlobalThemeAdmin,GrobalTheme
 # Register your models here.
 admin.site.register(Task, TaskAdmin)
 admin.site.register(Contest, ContestAdmin)
 admin.site.register(Theme, ThemeAdmin)
 admin.site.register(Solution)
+admin.site.register(GrobalTheme, GlobalThemeAdmin)

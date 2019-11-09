610584a03b5957f12dce2c586c96445b1b179849
CoffeBee <40368492+CoffeBee@users.noreply.github.com>
2019-11-09 14:46:34 +0300

    fix index.html

--- userprofile/models.py
+++ userprofile/models.py
@@ -5,3 +5,4 @@ class SupInformation(models.Model):
     organiration = models.CharField(max_length=100)
     grade = models.CharField(default="None", max_length=100)
     photo = models.ImageField()
+

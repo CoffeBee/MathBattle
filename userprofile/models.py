6282aefe6060ba6f74f7d894dea5dda5a21fec63
by_sm <by.sm@protonmail.com>
2019-11-10 11:01:10 +0300

    add userprofile model

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,8 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from tasks.models import Theme
 
 
-class SupInformation(models.Model):
-    organiration = models.CharField(max_length=100)
-    grade = models.CharField(default="None", max_length=100)
-    photo = models.ImageField()
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50, blank=True)
+    second_name = models.CharField(max_length=50, blank=True)
+    father_name = models.CharField(max_length=50, blank=True)
+    school = models.TextField(max_length=100, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    grade = models.IntegerField(null=True, blank=True)
 
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()

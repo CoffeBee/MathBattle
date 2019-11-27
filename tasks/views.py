ee7bb95a67a06e8f357952708a23e67a957cbc34
CoffeBee <40368492+CoffeBee@users.noreply.github.com>
2019-11-27 11:11:07 +0300

    add preview

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth.decorators import login_required
@@ -15,6 +15,7 @@ def task(request, task_title):
      if  request.method == 'POST':
         form = TaskForm(request.POST)
         if (form.is_valid()):
+
             ans = form.cleaned_data['answer']
             description = form.cleaned_data['description']
             checker = task.checker
@@ -23,12 +24,13 @@ def task(request, task_title):
                 rang = Rang.objects.get(user=request.user, theme=task.theme_set.all()[0].general_theme.all()[0]).point
             except:
                  pass
-
+            if "preview" in request.POST:
+                newsol = Solution(username=request.user, answer=ans, description=description, verdict=Virdict.PREVIEW, task=task, need_rang=rang)
+                newsol.save()
+                return redirect('../../themes/solutions/{}'.format(newsol.id))
             if checker.checkAns(ans, task.right_answer):
              newsol = Solution(username=request.user, answer=ans, description=description, verdict=Virdict.ACCEPTED_FOR_EVUALETION, task=task, need_rang=rang)
-             newsol = Solution(username=request.user, answer=ans, description=description, verdict=Virdict.ACCEPTED_FOR_EVUALETION, task=task, need_rang=rang)
             else:
-             newsol = Solution(username=request.user, answer=ans, description=description, verdict=Virdict.WRONG_ANSWER, task=task, need_rang=rang)
              newsol = Solution(username=request.user, answer=ans, description=description, verdict=Virdict.WRONG_ANSWER, task=task, need_rang=rang)
             newsol.save()
      return render(request, 'contest/task.html', context={'task' : task, 'form' : TaskForm(), 'submits' : submits})

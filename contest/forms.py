c895b6d2f63804d1da2188f6330dbdcbfab699bb
CoffeBee <40368492+CoffeBee@users.noreply.github.com>
2019-11-27 18:28:15 +0300

    fix

--- contest/forms.py
+++ contest/forms.py
@@ -10,5 +10,5 @@ class ContestRegister(forms.Form):
         super(ContestRegister, self).__init__(*args, **kwargs)
         self.fields['team'] = forms.ChoiceField(
             choices=[(o.id, o.name) for o in user.team_set.all()],
-            widget=forms.Select(attrs={'class' : 'custom-select form-control', 'style' : 'margin : 30px; margin-top : 10px; margin-bottom : 10px;'})
+            widget=forms.Select(attrs={'class' : 'custom-select form-control', 'style' : 'width : 40%;'})
         )

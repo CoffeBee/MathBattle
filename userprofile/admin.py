906203fbc87089c763fdfea80c29c2c9c46727d7
uliana2006eskova <uliana.eskova@gmail.com>
2020-10-15 22:51:03 +0300

    add table score

--- userprofile/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Team, Profile
-# Register your models here.
-admin.site.register(Team)
-admin.site.register(Profile)
--- userprofile/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', profile_views),
-    path('/team/<str:team_name>', team),
-    path('/update_profiles', update_profile),
-]
